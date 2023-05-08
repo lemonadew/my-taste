213d9c6967988a7c3410f760f529fa98c0fd7ca0
lemonadeUwUs <basenjisniper@gmail.com>
2023-05-08 19:42:24 -0400

    Begin frontend development

--- icecream/src/App.tsx
+++ icecream/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './kek.jpg';
 import './App.css';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const theButton = document.querySelector(".button");
+
+async function boop() {
+  await sleep(5000);
+  theButton?.classList.remove("button--loading");
+}
+
+theButton?.addEventListener("click", () => {
+    theButton?.classList.add("button--loading");
+    boop();
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "App">
+      <header className = "Logo">
+        <h2 data-text = "My Taste"> My Taste</h2>
       </header>
+      
+      <button type= "button" className="button generate">
+        <span className='button__text'>Let's Cook!</span>
+      </button>
     </div>
   );
 }
