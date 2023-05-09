8a434632f832ff00f5a11e3b3531c14223f7ed28
lemonadeUwUs <basenjisniper@gmail.com>
2023-05-09 18:26:41 -0400

    fleshing out the layout

--- icecream/src/App.tsx
+++ icecream/src/App.tsx
@@ -3,15 +3,15 @@ import logo from './kek.jpg';
 import './App.css';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-const theButton = document.querySelector(".button");
+const genButton = document.querySelector(".button");
 
 async function boop() {
   await sleep(5000);
-  theButton?.classList.remove("button--loading");
+  genButton?.classList.remove("button--loading");
 }
 
-theButton?.addEventListener("click", () => {
-    theButton?.classList.add("button--loading");
+genButton?.addEventListener("click", () => {
+    genButton?.classList.add("button--loading");
     boop();
 });
 
@@ -20,12 +20,31 @@ function App() {
   return (
     <div className = "App">
       <header className = "Logo">
-        <h2 data-text = "My Taste"> My Taste</h2>
+        <h2 data-text = "My Taste">My Taste</h2>
       </header>
+
+      <div className = "ingredient-container">
+        <h3 className="ingredient-list-title">Your Ingredients</h3>
+        <ul className = "ingredient-list">
+          <li>Mushrooms</li>
+        </ul>
+        <form action="">
+          <input 
+            type="text"
+            className="new-ingredient-list"
+            placeholder="Add a new ingredient!"
+            aria-label="Add a new ingredient"
+          />
+          <button className="ingredient-btn" aria-label='create a new ingredient'>+</button>
+        </form>
+      </div> 
       
-      <button type= "button" className="button generate">
-        <span className='button__text'>Let's Cook!</span>
-      </button>
+      <div className="generate">
+        <button type= "button" className="button generate">
+          <span className='button__text'>Let's Cook!</span>
+        </button>
+      </div>
+      
     </div>
   );
 }
